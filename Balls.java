d98f0189789409b299f9da1d7aa792278b24bd25
Nuttapon Pattanavijit <nuttt.p@gmail.com>
2012-10-04 23:24:38 +0700

    fix bounce error

--- Balls.java
+++ Balls.java
@@ -42,8 +42,8 @@ public class Balls implements Runnable {
 				/*
 				 *  Y Bounce
 				 */
-				if (b.getY() - b.getRadius() < 0
-						|| b.getY() + b.getRadius() > gui.getGUIHeight()) {
+				if (b.getY() - b.getRadius() < 0 && b.getDY() < 0
+						|| b.getY() + b.getRadius() > gui.getGUIHeight() && b.getDY() > 0) {
 					b.setDY(-b.getDY());
 				}
 				/*
@@ -76,6 +76,12 @@ public class Balls implements Runnable {
 						else
 							phi2 = (phi+theta)/2;
 					}
+					if(phi > Math.toRadians(MAXIMUM_ANGLE))
+						phi = Math.toRadians(MAXIMUM_ANGLE);
+					if(phi < Math.toRadians(-MAXIMUM_ANGLE))
+						phi = Math.toRadians(-MAXIMUM_ANGLE);
+					if(Math.abs(phi) < Math.toRadians(5))
+						phi = Math.random()*10-5;
 //					System.out.println("phi2: "+Math.toDegrees(phi2));
 					double v = Math.sqrt(Math.pow(b.getDX(), 2)+Math.pow(b.getDY(), 2));
 					b.setDX(Math.cos(phi2)*v);
